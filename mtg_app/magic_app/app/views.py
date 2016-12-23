e1a8b8ef2a15931e631d421eea4542f08a2ca805
Kaushik Vasudevan <KVASU@Kaushiks-MacBook-Pro.local>
2016-12-23 14:37:56 -0800

    Adding dropdown selection for Magic Cards

--- mtg_app/magic_app/app/views.py
+++ mtg_app/magic_app/app/views.py
@@ -1,7 +1,7 @@
 import logging
 import json
 
-from flask import render_template
+from flask import render_template, request, jsonify
 from flask_wtf import Form
 from wtforms import fields
 from wtforms.validators import Required
@@ -14,12 +14,29 @@ from . import app
 
 logger = logging.getLogger('app')
 
-class PredictForm(Form):
+CARDS_DF = pd.read_csv("app/data/Magic_Pandas_DF.csv")
 
-    magic_card = fields.StringField('Select a Magic Card (Through Kaladesh):', validators=[Required()])
+class PredictForm(Form):
+    magic_card = fields.SelectField('Select a Magic Card (Through Kaladesh):', 
+        choices=[],
+        validators=[Required()])
   
     submit = fields.SubmitField('Submit')
 
+
+@app.route("/search")
+def search():
+    print(request.args)
+    query = str(request.args.get("q"))
+    print(query)
+    # query = 
+    mask = CARDS_DF["name"].str.contains(query, case=False)
+    print(mask.sum())
+    items = CARDS_DF.loc[mask, "name"][:100].tolist()
+    print(items)
+
+    name_list = [{"text": item, "id": item} for item in items]
+    return jsonify({"results": name_list})
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
